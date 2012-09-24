46ef8ea02f097801af290ca9b62e694cd6d97aa4
Guillaume Aubert <aubertg@cpan.org>
2012-09-24 01:01:42 -0400

    Released version 1.1.0.

--- lib/Test/Dist/VersionSync.pm
+++ lib/Test/Dist/VersionSync.pm
@@ -14,11 +14,11 @@ Test::Dist::VersionSync - Verify that all the modules in a distribution have the
 
 =head1 VERSION
 
-Version 1.0.4
+Version 1.1.0
 
 =cut
 
-our $VERSION = '1.0.4';
+our $VERSION = '1.1.0';
 
 
 =head1 SYNOPSIS

c70a3c511d4becd0b2d0fec8e289cda842c4b7a4
Guillaume Aubert <aubertg@cpan.org>
2013-05-08 22:32:00 -0400

    Updated copyright year.

--- lib/Test/Dist/VersionSync.pm
+++ lib/Test/Dist/VersionSync.pm
@@ -314,7 +314,7 @@ L<http://search.cpan.org/dist/test-dist-versionsync/>
 
 =head1 COPYRIGHT & LICENSE
 
-Copyright 2012 Guillaume Aubert.
+Copyright 2012-2013 Guillaume Aubert.
 
 This program is free software: you can redistribute it and/or modify it under
 the terms of the GNU General Public License version 3 as published by the Free

f046b36247d6708659274707fa0461aaf5426e6e
Guillaume Aubert <aubertg@cpan.org>
2013-11-15 18:52:04 -0800

    Whitespace cleanup.

--- lib/Test/Dist/VersionSync.pm
+++ lib/Test/Dist/VersionSync.pm
@@ -32,12 +32,12 @@ our $VERSION = '1.1.2';
 The most common use should be to add a module_versions.t file to your tests directory for a given distribution, with the following content:
 
 	#!perl -T
-	
+
 	use strict;
 	use warnings;
-	
+
 	use Test::More;
-	
+
 	# Ensure a recent version of Test::Dist::VersionSync
 	my $version_min = '1.0.1';
 	eval "use Test::Dist::VersionSync $version_min";
@@ -55,7 +55,7 @@ number.
 
 	# Default, use MANIFEST and MANIFEST.SKIP to find out what modules exist.
 	ok_versions();
-	
+
 	# Optional, specify a list of modules to check for identical versions.
 	ok_versions(
 		modules =>
@@ -73,19 +73,19 @@ sub ok_versions
 	my ( %args ) = @_;
 	my $modules = delete( $args{'modules'} );
 	my $return = 1;
-	
+
 	# Find out via Test::Builder if a plan has been declared, otherwise we'll
 	# declare our own.
 	my $builder = Test::More->builder();
 	my $plan_declared = $builder->has_plan();
-	
+
 	# If a list of files was passed, verify that the argument is an arrayref.
 	# Otherwise, load the files from MANIFEST and MANIFEST.SKIP.
 	if ( defined( $modules) )
 	{
 		Test::More::plan( tests => 3 )
 			unless $plan_declared;
-		
+
 		$return = Test::More::isa_ok(
 			$modules,
 			'ARRAY',
@@ -96,10 +96,10 @@ sub ok_versions
 	{
 		Test::More::plan( tests => 5 )
 			unless $plan_declared;
-		
+
 		$modules = _get_modules_from_manifest();
 	}
-	
+
 	# If we have modules, check their versions.
 	SKIP:
 	{
@@ -107,33 +107,33 @@ sub ok_versions
 			'No module found in the distribution.',
 			2,
 		) if scalar( @$modules ) == 0;
-		
+
 		my $versions = {};
 		$return = Test::More::subtest(
 			'Retrieve versions for all modules listed.',
 			sub
 			{
 				Test::More::plan( tests => scalar( @$modules ) * 2 );
-				
+
 				foreach my $module ( @$modules )
 				{
 					Test::More::use_ok( $module );
-					
+
 					my $version = $module->VERSION();
 					my $version_declared = Test::More::ok(
 						defined( $version ),
 						"Module $module declares a version.",
 					);
-					
+
 					$version = '(undef)'
 						unless $version_declared;
-					
+
 					$versions->{ $version } ||= [];
 					push( @{ $versions->{ $version } }, $module );
 				}
 			}
 		) && $return;
-		
+
 		my $has_only_one_version = is(
 			scalar( keys %$versions ),
 			1,
@@ -142,7 +142,7 @@ sub ok_versions
 		diag( 'Versions and the modules they were found in: ' . Dumper( $versions ) )
 			unless $has_only_one_version;
 		$return = $has_only_one_version && $has_only_one_version;
-		
+
 	}
 
 	return $return;
@@ -154,7 +154,7 @@ sub ok_versions
 Import a test plan. This uses the regular Test::More plan options.
 
 	use Test::Dist::VersionSync tests => 4;
-	
+
 	ok_versions();
 
 Test::Dist::VersionSync also detects if Test::More was already used with a test
@@ -162,7 +162,7 @@ plan declared and will piggyback on it. For example:
 
 	use Test::More tests => 2;
 	use Test::Dist::VersionSync;
-	
+
 	ok( 1, 'Some Test' );
 	ok_versions();
 
@@ -171,10 +171,10 @@ plan declared and will piggyback on it. For example:
 sub import
 {
 	my ( $self, %test_plan ) = @_;
-	
+
 	Test::More::plan( %test_plan )
 		if scalar( keys %test_plan ) != 0;
-	
+
 	return 1;
 }
 
@@ -205,7 +205,7 @@ sub _get_modules_from_manifest
 			open( my $MANIFESTSKIP, '<', 'MANIFEST.SKIP' ),
 			'Retrieve MANIFEST.SKIP file.',
 		) || diag( "Failed to open < MANIFEST.SKIP file: $!." );
-		
+
 		if ( $opened_manifest_skip )
 		{
 			my $exclusions = [];
@@ -215,7 +215,7 @@ sub _get_modules_from_manifest
 				push( @$exclusions, $pattern );
 			}
 			close( $MANIFESTSKIP );
-			
+
 			$excluded_patterns = '(' . join( '|', @$exclusions ) . ')'
 				if scalar( @$exclusions ) != 0;
 		}
@@ -227,14 +227,14 @@ sub _get_modules_from_manifest
 			'No MANIFEST.SKIP found, skipping.',
 		);
 	}
-	
+
 	# Make sure that there is a MANIFEST file at the root of the distribution,
 	# before we even open it.
 	my $manifest_exists = Test::More::ok(
 		-e 'MANIFEST',
 		'The MANIFEST file is present at the root of the distribution.',
 	);
-	
+
 	# Retrieve the list of modules in MANIFEST.
 	my $modules = [];
 	SKIP:
@@ -243,12 +243,12 @@ sub _get_modules_from_manifest
 			'MANIFEST is missing, cannot retrieve list of files.',
 			1,
 		) unless $manifest_exists;
-	
+
 		my $opened_manifest = Test::More::ok(
 			open( my $MANIFEST, '<', 'MANIFEST' ),
 			'Retrieve MANIFEST file.',
 		) || diag( "Failed to open < MANIFEST file: $!." );
-		
+
 		if ( $opened_manifest )
 		{
 			while ( my $file = <$MANIFEST> )
@@ -256,7 +256,7 @@ sub _get_modules_from_manifest
 				chomp( $file );
 				next if defined( $excluded_patterns ) && $file =~ /$excluded_patterns/;
 				next unless $file =~ m/^lib[\\\/](.*)\.pm$/;
-				
+
 				my $module = $1;
 				$module =~ s/[\\\/]/::/g;
 				push( @$modules, $module );
@@ -264,7 +264,7 @@ sub _get_modules_from_manifest
 			close( $MANIFEST );
 		}
 	}
-	
+
 	return $modules;
 }
 
